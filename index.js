bdc6983a7bf17d8e8fd56cf842367922492e7737
“Rose <RoseLCromwell32@Gmail.Com>
2023-04-06 12:01:26 -0400

    Prototypes Answers

--- index.js
+++ index.js
@@ -15,65 +15,94 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
-}
-
-
-/*
-  TASK 2
-    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
-    - All instances built with Car:
-        + should initialize with an `tank` at 0
-        + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method
-      + should take 'gallons' as an parameter which will take number of gallons as an argument
-      + should add the gallons to `tank`.
-    - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
-        + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
-        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
-*/
-
-function Car() {
-
-}
-
-
-/*
-  TASK 3
-    - Write a Baby constructor subclassing Person.
-    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
-    - Besides the methods on Person.prototype, babies also have the ability to `.play()`:
-        + Should return a string "Playing with x", x being the favorite toy.
-*/
-
-function Baby() {
-
-}
-
-
-/* 
-  TASK 4
-  In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
-*/
-
-///////// END OF CHALLENGE /////////
-
-/* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working!');
-  return 'bar';
-}
-foo();
-module.exports = {
-  foo,
-  Person, 
-  Car,
-  Baby
-}
+function Person(name, age) {
+  this.name = name
+  this.age = age
+  this.stomach = []
+  }
+  
+  Person.prototype.eat = function(someFood) {
+    if (this.stomach.length < 10);
+    return this.stomach.push(someFood);
+  }
+  
+  Person.prototype.poop = function() {
+    return this.stomach = []
+  }
+  
+  Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`
+  }
+  
+  /*
+    TASK 2
+      - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+      - All instances built with Car:
+          + should initialize with an `tank` at 0
+          + should initialize with an `odometer` at 0
+      - Give cars the ability to get fueled with a `.fill(gallons)` method
+        + should take 'gallons' as an parameter which will take number of gallons as an argument
+        + should add the gallons to `tank`.
+      - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
+          + Should cause the `odometer` to go up.
+          + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+      - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
+          + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+  */
+  
+  function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
+  }
+  
+   Car.prototype.fill = function(gallons) {
+      this.tank = this.tank + gallons
+   }
+  
+   console.log(Car.prototype.fill(10))
+  
+  /*
+    TASK 3
+      - Write a Baby constructor subclassing Person.
+      - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+      - Besides the methods on Person.prototype, babies also have the ability to `.play()`:
+          + Should return a string "Playing with x", x being the favorite toy.
+  */
+  
+  function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age)
+    this.favoriteToy = favoriteToy
+  }
+  
+  Baby.prototype = Object.create(Person.prototype);
+  
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`
+  }
+  
+  /* 
+    TASK 4
+    In your own words explain the four principles for the "this" keyword below:
+    1. Global Binding/Window Binding - if all else fails, returns information in the window of a browser.
+    2. Implicit binding - Uses dot syntax to imply a connection.
+    3. Explicit binding - Uses functions to make a connection.
+    4. New binding - Uses the "new" keyword to create a new object based on another one.
+  */
+  
+  ///////// END OF CHALLENGE /////////
+  
+  /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
+  function foo(){
+    console.log('its working!');
+    return 'bar';
+  }
+  foo();
+  module.exports = {
+    foo,
+    Person, 
+    Car,
+    Baby
+  }
+  
